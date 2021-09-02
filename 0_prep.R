ec213db95aa25c2ab9a96a87d697fae5009b2f40
Ariann Schmid <ariannaschmid96@gmail.com>
2021-09-02 12:11:54 -0400

    2nd commit

--- 0_prep.R
+++ 0_prep.R
@@ -1,2 +1,9 @@
 #code code code
 x <- 5
+install.packages("usethis")
+library(usethis)
+create_github_token() #this opens up a link to github
+gitcreds::gitcreds_set() #paste the token after running this
+
+y<-3
+z<-4
